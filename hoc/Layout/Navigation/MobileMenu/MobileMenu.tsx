bfbcb8518ad49eb188aba317e0b0013cd18ee772
jakub0227 <jakub.wysocki.1983@gmail.com>
2021-12-07 18:13:16 +0100

    fix mobilenav, add drawer

--- hoc/Layout/Navigation/MobileMenu/MobileMenu.tsx
+++ hoc/Layout/Navigation/MobileMenu/MobileMenu.tsx
@@ -9,57 +9,82 @@ import {
   CloseButton,
   Icon,
   useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  HStack,
+  StackDivider,
 } from "@chakra-ui/react";
 import { HiMenu } from "react-icons/hi";
 import { DarkModeSwitch } from "../../../../components/DarkModeSwitch";
 
 export const MobileMenu = () => {
   const bg = useColorModeValue("white", "gray.800");
-  const mobileNav = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box display={{ base: "inline-flex", md: "none" }}>
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        aria-label="Open menu"
-        fontSize="20px"
-        color={useColorModeValue("gray.800", "inherit")}
-        variant="ghost"
-        icon={<Icon as={HiMenu} />}
-        onClick={mobileNav.onOpen}
-      />
-      <VStack
-        pos="absolute"
-        top={0}
-        left={0}
-        right={0}
-        display={mobileNav.isOpen ? "flex" : "none"}
-        flexDirection="column"
-        p={2}
-        pb={4}
-        m={2}
-        bg={bg}
-        spacing={3}
-        rounded="sm"
-        shadow="sm"
-      >
-        <CloseButton aria-label="Close menu" onClick={mobileNav.onClose} />
-        <NextLink href="/">
-          <Button w="full" variant="ghost">
-            Home
-          </Button>
-        </NextLink>
-        <NextLink href="/weather">
-          <Button w="full" variant="ghost">
-            Weather
-          </Button>
-        </NextLink>
-        <NextLink href="/about">
-          <Button w="full" variant="ghost">
-            About
-          </Button>
-        </NextLink>
-        <DarkModeSwitch />
-      </VStack>
-    </Box>
+    <>
+      <Box display={{ base: "inline-flex", md: "none" }}>
+        <IconButton
+          display={{ base: "flex", md: "none" }}
+          aria-label="Open menu"
+          fontSize="20px"
+          color={useColorModeValue("gray.800", "inherit")}
+          variant="ghost"
+          icon={<Icon as={HiMenu} />}
+          onClick={onOpen}
+        />
+        <Drawer onClose={onClose} isOpen={isOpen} placement="top" size="xs">
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader>Menu</DrawerHeader>
+            <VStack
+              divider={<StackDivider borderColor="gray.200" />}
+              display={isOpen ? "flex" : "none"}
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              p={2}
+              pb={4}
+              m={2}
+              bg={bg}
+              spacing={3}
+              rounded="sm"
+              shadow="sm"
+            >
+              <DrawerBody>
+                <NextLink href="/">
+                  <Button w="full" variant="ghost">
+                    Home
+                  </Button>
+                </NextLink>
+                <NextLink href="/weather">
+                  <Button w="full" variant="ghost">
+                    Weather
+                  </Button>
+                </NextLink>
+                <NextLink href="/about">
+                  <Button w="full" variant="ghost">
+                    About
+                  </Button>
+                </NextLink>
+              </DrawerBody>
+              <DrawerFooter>
+                <HStack spacing={8} width="100%">
+                  <CloseButton
+                    aria-label="Close menu"
+                    onClick={onClose}
+                    color="teal.600"
+                  />
+                  <DarkModeSwitch />
+                </HStack>
+              </DrawerFooter>
+            </VStack>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </>
   );
 };
